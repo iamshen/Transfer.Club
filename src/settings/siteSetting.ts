d0b0d82f242e58feb9d942f3b625eb6200ee2fee
黄深 <lzzyjsj@gmail.com>
2021-03-24 15:45:15 +0800

    Initial commit

---
+++ src/settings/siteSetting.ts
@@ -0,0 +1,6 @@
+// github repo url
+export const GITHUB_URL = 'https://github.com/i66soft/osharp-vben-template';
+// vue-vben-admin-next-doc
+export const DOC_URL = 'https://vvbin.cn/doc-next/';
+// site url
+export const SITE_URL = 'https://vvbin.cn/next/';
